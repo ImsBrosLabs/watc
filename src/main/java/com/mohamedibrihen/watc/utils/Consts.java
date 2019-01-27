1da90f0312dc96cb3be9f863b82a8a25cc463e5a
Mohamed IBRIHEN <med.ibrihen@gmail.com>
2019-01-28 00:46:50 +0100

    feat() : TODO's for getting stingers data + some code formating

--- src/main/java/com/mohamedibrihen/watc/utils/Consts.java
+++ src/main/java/com/mohamedibrihen/watc/utils/Consts.java
@@ -4,14 +4,16 @@ public final class Consts {
 
     private static final String SEARCH_QUERY_STRING= "?s=";
     private static final String AFTER_CREDITS_URL = "http://aftercredits.com/";
-
     // TODO : use a different user agent to bypass the limitation ?
     private static final String MEDIA_STINGER_URL = "http://www.mediastinger.com/";
 
+    public static final int LIMIT_VALUE = 3;
+
     public static final String AFTER_CREDITS_SEARCH_URL_TEMPLATE = AFTER_CREDITS_URL + SEARCH_QUERY_STRING;
     public static final String MEDIA_STINGER_SEARCH_URL_TEMPLATE = MEDIA_STINGER_URL + SEARCH_QUERY_STRING;
-    public static final int LIMIT_VALUE = 3;
-    public static final String AFTERCREDITS_XPATH_ELEMENT_QUERY = "//div[contains(@class, 'td-module-thumb')]";
+
+    public static final String AFTERCREDITS_SEARCH_XPATH_ELEMENT_QUERY = "//div[contains(@class, 'td-module-thumb')]";
+
 
 
     /**
