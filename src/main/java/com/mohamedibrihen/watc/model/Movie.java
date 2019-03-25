02933497f3121755e6106d0afc6fb8c60773b106
Mohamed IBRIHEN <med.ibrihen@gmail.com>
2019-03-25 17:15:57 +0100

    add posterUrl to Movie

--- src/main/java/com/mohamedibrihen/watc/model/Movie.java
+++ src/main/java/com/mohamedibrihen/watc/model/Movie.java
@@ -3,6 +3,7 @@ package com.mohamedibrihen.watc.model;
 import com.mohamedibrihen.watc.model.enums.Source;
 import lombok.Data;
 
+import java.net.URL;
 import java.time.LocalDate;
 
 @Data
@@ -10,6 +11,7 @@ public class Movie {
     private String title;
     private Source source;
     private LocalDate releaseDate;
+    private URL posterUrl;
     private boolean hasExtrasDuringCredits;
     private boolean hasStinger;
 }
