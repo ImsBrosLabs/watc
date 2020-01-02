d589df54688c1666fbafee7d4dce693c0e08ee8e
Mohamed IBRIHEN <mohamed.ibrihen@softeam.fr>
2020-01-02 16:18:35 +0100

    clean unused imports and change groupId value

--- src/main/java/com/imslabs/watc/WatcApplication.java
+++ src/main/java/com/imslabs/watc/WatcApplication.java
@@ -1,9 +1,7 @@
 package com.imslabs.watc;
 
-import com.gargoylesoftware.htmlunit.WebClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WatcApplication {
